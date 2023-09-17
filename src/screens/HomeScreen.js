e80d1fc8d092067ab037ead1b0f6df7ac57c0214
Hoangminh249 <97183505+Hoangminh249@users.noreply.github.com>
2023-09-17 16:03:32 +0700

    innit project

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View } from "react-native";
+
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text>
+                HomeScreen
+            </Text>
+        </View>
+    )
+}
